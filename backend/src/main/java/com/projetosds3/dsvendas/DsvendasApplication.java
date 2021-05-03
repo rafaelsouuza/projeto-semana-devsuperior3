c5bf7dafa505cce4d853ed974108e2132787efe9
Rafael Souza <snrafa.souza@gmail.com>
2021-05-03 20:38:47 -0300

    Projetos criados

---
+++ backend/src/main/java/com/projetosds3/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.projetosds3.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetosds3/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosds3.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
